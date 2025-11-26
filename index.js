67d8e039e9783d3cf9e9bfb93c67e2602a209856
shah9192 <shakirhassantech@gmail.com>
2025-11-26 22:49:49 +0500

    deployed link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL (adjust as needed)
+    origin: "*", // Frontend URL (adjust as needed)
     // methods: 'GET,POST,PUT,DELETE',
     // allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Allow cookies (including admin token)
